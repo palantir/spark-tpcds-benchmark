17dea8f196b1f79d8db6fa6c8aee5692bf3c6ba0
Will Manning <wmanning@palantir.com>
2020-06-30 18:27:01 +0100

    better logging

--- spark-tpcds-benchmark-runner/src/main/java/com/palantir/spark/benchmark/metrics/BenchmarkMetrics.java
+++ spark-tpcds-benchmark-runner/src/main/java/com/palantir/spark/benchmark/metrics/BenchmarkMetrics.java
@@ -73,7 +73,9 @@ public final class BenchmarkMetrics {
                         JacksonSerializer.create(BenchmarkMetric.class))
                 .createOrOpen();
         if (!this.metricsBuffer.isEmpty()) {
-            log.warn("Found unflushed metrics in the buffer; attempting to flush");
+            log.warn(
+                    "Found unflushed {} metrics in the buffer; attempting to flush",
+                    SafeArg.of("numMetrics", metricsBuffer.size()));
             flushMetrics();
         }
     }
